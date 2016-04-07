5ba6143455b31b18c59ab0b7f3939e3f3e61e3a9
Prasanthi Relangi <prasanthi.relangi@gmail.com>
2016-04-07 00:00:17 -0700

    Add support for two cards
    Refactor code to be used by both cards

--- DayOut/ViewController.swift
+++ DayOut/ViewController.swift
@@ -11,10 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var card1ImageView: UIImageView!
-    
+    @IBOutlet weak var card1Background: UIImageView!
+    @IBOutlet weak var card2ImageView: UIImageView!
+    @IBOutlet weak var card2Background: UIImageView!
     @IBOutlet weak var likeButton: UIButton!
     
     @IBOutlet weak var cancelButton: UIButton!
+    var currentCardIndex:Int = 0
     
     
     @IBOutlet var panGestureRecognizer: UIPanGestureRecognizer!
@@ -37,60 +40,102 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onLike(sender: AnyObject) {
+        doLikeAnimation()
+    }
+
+    @IBAction func onCancel(sender: AnyObject) {
+        doCancelAnimation()
+    }
+    
+    func doCancelAnimation() {
+        var cardView: UIImageView!
+        var cardBackgroundView: UIImageView!
+        
+        if currentCardIndex == 0 {
+            cardView = card1ImageView
+            cardBackgroundView = card1Background
+        }
+        else {
+            cardView = card2ImageView
+            cardBackgroundView = card2Background
+        }
+        
+        cardView.hidden = false
         UIView.animateWithDuration(1.0, animations: { () -> Void in
-            self.card1ImageView.center = CGPoint(x: self.originalCenter.x+250, y: self.originalCenter.y)
-            let angle =  CGFloat(45 * M_PI/180)
-            self.card1ImageView.transform = CGAffineTransformRotate(self.originalTransform, angle)
+            
+            self.originalCenter = cardView.center
+            self.originalTransform = cardView.transform
+            cardView.center = CGPoint(x: self.originalCenter.x-250, y: self.originalCenter.y)
+            let angle =  CGFloat(-45 * M_PI/180)
+            cardView.transform = CGAffineTransformRotate(self.originalTransform, angle)
             
             }) { (finished:Bool) -> Void in
                 if finished == true {
-                    self.card1ImageView.hidden = true
+                    cardView.hidden = true
+                    cardBackgroundView.hidden = true
+                    if self.currentCardIndex == 1 {
+                        self.likeButton.hidden = true
+                        self.cancelButton.hidden = true
+                    }
+                    self.currentCardIndex += 1
                 }
                 
                 
         }
         
-    }
-
-    @IBAction func onCancel(sender: AnyObject) {
-        
-        card1ImageView.hidden = false
-        card1ImageView.center = originalCenter
-        card1ImageView.transform = originalTransform
-        
-//        self.card1ImageView.hidden = false
-//        UIView.animateWithDuration(1.0, animations: { () -> Void in
-//            self.card1ImageView.center = CGPoint(x: self.originalCenter.x-250, y: self.originalCenter.y)
-//            let angle =  CGFloat(-45 * M_PI/180)
-//            self.card1ImageView.transform = CGAffineTransformRotate(self.originalTransform, angle)
-//            
-//            }) { (finished:Bool) -> Void in
-//                if finished == true {
-//                    self.card1ImageView.hidden = true
-//                }
-//                
-//                
-//        }
-        
-        
         
     }
     
-    @IBAction func onSwipe(sender: UIPanGestureRecognizer) {
+    func doLikeAnimation() {
         
-        print("Panning")
+        var cardView: UIImageView!
+        var cardBackgroundView: UIImageView!
         
+        if currentCardIndex == 0 {
+            cardView = card1ImageView
+            cardBackgroundView = card1Background
+        }
+        else {
+            cardView = card2ImageView
+            cardBackgroundView = card2Background
+        }
+        
+        UIView.animateWithDuration(1.0, animations: { () -> Void in
+            self.originalCenter = cardView.center
+            self.originalTransform = cardView.transform
+            
+            cardView.center = CGPoint(x: self.originalCenter.x+250, y: self.originalCenter.y)
+            let angle =  CGFloat(45 * M_PI/180)
+            cardView.transform = CGAffineTransformRotate(self.originalTransform, angle)
+            
+            }) { (finished:Bool) -> Void in
+                if finished == true {
+                    cardView.hidden = true
+                    cardBackgroundView.hidden = true
+                    
+                    if self.currentCardIndex == 1 {
+                        self.likeButton.hidden = true
+                        self.cancelButton.hidden = true
+                    }
+                    self.currentCardIndex += 1
+                }
+        }
+    }
+    
+    
+    
+    func doTinderAnimation(sender: UIPanGestureRecognizer, cardView: UIImageView, cardBackgroundView: UIImageView) {
         let translation = sender.translationInView(view)
         let point = sender.locationInView(view)
         
         if sender.state == UIGestureRecognizerState.Began {
             //originalCenter =
-            originalCenter = card1ImageView.center
-            originalTransform = card1ImageView.transform
+            originalCenter = cardView.center
+            originalTransform = cardView.transform
             
         }
         else if sender.state == UIGestureRecognizerState.Changed {
-            card1ImageView.center = CGPoint(x: self.originalCenter.x + sender.translationInView(view).x, y: originalCenter.y)
+            cardView.center = CGPoint(x: self.originalCenter.x + sender.translationInView(view).x, y: originalCenter.y)
             
             var multiplier = 1.0
             if point.y < view.frame.height/2 {
@@ -102,19 +147,38 @@ class ViewController: UIViewController {
             let xOffset = translation.x
             let angle = CGFloat(multiplier * 1 * M_PI/180) / 20 * xOffset
             
-            self.card1ImageView.transform = CGAffineTransformRotate(originalTransform, angle)
+            cardView.transform = CGAffineTransformRotate(originalTransform, angle)
         }
         else if sender.state == UIGestureRecognizerState.Ended {
             if translation.x > 100 || translation.x < -100 {
-                self.card1ImageView.hidden = true
+                cardView.hidden = true
+                cardBackgroundView.hidden = true
+                currentCardIndex += 1
             }
             else {
-                self.card1ImageView.transform = originalTransform
-                self.card1ImageView.center = originalCenter
+                cardView.transform = originalTransform
+                cardView.center = originalCenter
             }
         }
 
+    }
+    
+    @IBAction func onCard2Swipe(sender: UIPanGestureRecognizer) {
         
+        //print("Card2 panning")
+        doTinderAnimation(sender, cardView: card2ImageView, cardBackgroundView: card2Background)
+        
+        likeButton.hidden = true
+        cancelButton.hidden = true
+        
+        
+    }
+    @IBAction func onSwipe(sender: UIPanGestureRecognizer) {
+        
+        //print("Panning")
+        doTinderAnimation(sender, cardView: card1ImageView,cardBackgroundView: card1Background)
+ 
+   
         
     }
 }
